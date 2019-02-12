a951cb0684d794eba31c6cd962a6276864f29178
CarlW <carl@oskarroger.net>
2019-02-12 19:38:39 +0800

    Added new SW portfolio item: Reach Weather App

---
+++ portfolioSites/react-weather-app/precache-manifest.afbf5eea4512737ff5837b0f5b27d26c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/portfolioSites/react-weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "be94e43b61625c731f36",
+    "url": "/portfolioSites/react-weather-app/static/js/runtime~main.be94e43b.js"
+  },
+  {
+    "revision": "4d4a096bc0d150dbdbdc",
+    "url": "/portfolioSites/react-weather-app/static/js/main.4d4a096b.chunk.js"
+  },
+  {
+    "revision": "66d6a41866e356116ff3",
+    "url": "/portfolioSites/react-weather-app/static/js/1.66d6a418.chunk.js"
+  },
+  {
+    "revision": "4d4a096bc0d150dbdbdc",
+    "url": "/portfolioSites/react-weather-app/static/css/main.b9a142dc.chunk.css"
+  },
+  {
+    "revision": "66d6a41866e356116ff3",
+    "url": "/portfolioSites/react-weather-app/static/css/1.a33fa1db.chunk.css"
+  },
+  {
+    "revision": "daf820161ba439b3a491dbe441e56310",
+    "url": "/portfolioSites/react-weather-app/index.html"
+  }
+];
