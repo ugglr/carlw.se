e3441a026e5fa43b9041684f2ea9d9c05853089f
CarlW <carl@oskarroger.net>
2019-03-01 12:18:48 +0800

    added new portfolio item. React Recipe API search App

---
+++ portfolioSites/react-recipe-app/precache-manifest.aeb3bdfb35d372d3c3b222fa77c1cf5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "871742f10f5c334c1ae5",
+    "url": "/portfolioSites/react-recipe-app/static/js/runtime~main.871742f1.js"
+  },
+  {
+    "revision": "db1e8447ab0664c50c79",
+    "url": "/portfolioSites/react-recipe-app/static/js/main.db1e8447.chunk.js"
+  },
+  {
+    "revision": "66b05adfc48c8d26d667",
+    "url": "/portfolioSites/react-recipe-app/static/js/2.66b05adf.chunk.js"
+  },
+  {
+    "revision": "db1e8447ab0664c50c79",
+    "url": "/portfolioSites/react-recipe-app/static/css/main.81e5175a.chunk.css"
+  },
+  {
+    "revision": "66b05adfc48c8d26d667",
+    "url": "/portfolioSites/react-recipe-app/static/css/2.bd7a68b1.chunk.css"
+  },
+  {
+    "revision": "63956059cb141915221ac21992711d37",
+    "url": "/portfolioSites/react-recipe-app/index.html"
+  }
+];
